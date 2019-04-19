72fd502fb3e6e4509d2287cd73e4297390629d71
jrtambosi <junior.tambosi@gmail.com>
2019-04-19 13:52:22 -0300

    aula04

---
+++ cucumber/portal-cursos/features/step_definitions/steps.rb
@@ -0,0 +1,7 @@
+Quando("eu faço uma busca pelo termo {string}") do |termo|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então("devo ver a seguinte notificação {string}") do |notificacao|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
