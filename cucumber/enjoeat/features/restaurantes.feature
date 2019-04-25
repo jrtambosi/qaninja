614fd0dbb7d05508a918764a0a4ffd4cf895cfab
VALMOR TAMBOSI JUNIOR <junior.tambosi@gmail.com>
2019-04-25 17:43:26 -0300

    parou317

--- cucumber/enjoeat/features/restaurantes.feature
+++ cucumber/enjoeat/features/restaurantes.feature
@@ -5,27 +5,32 @@ Funcionalidade: Restaurantes
     Sendo um usuário que deseja pedir comida
     Posso acessar a lista de restaurantes
 
-    
-    Cenario: Todos os restaurantes
+    Contexto: Lista de restaurantes
 
+        Dado que tenho uma lista de restaurantes
+
+    Cenario: Todos os restaurantes
+        
         Quando acesso a lista de restaurantes
         Então vejo todas as opções disponíveis
-    @temp
+    
     Cenario: Categorias
-
+        
         Quando acesso a lista de restaurantes
         Então cada restaurante deve exibir sua categoria
 
     Cenario: Tempo de entrega
-
+        
         Quando acesso a lista de restaurantes
         Então cada restaurante deve exibir o tempo de entrega
 
     Cenario: Nota de avaliação
-
+        
         Quando acesso a lista de restaurantes
         Então cada restaurante deve exibir sua nota de avaliação
 
+    # Scenario Outline ou Data Tables?
+        
 
 
         
--- cucumber/enjoeat/features/step_definitions/restaurantes_steps.rb
+++ cucumber/enjoeat/features/step_definitions/restaurantes_steps.rb
@@ -1,27 +1,81 @@
 # You can implement step definitions for undefined steps with these snippets:
 # tempo video 21,31
 
+
+Dado("que tenho uma lista de restaurantes") do
+
+    @restaurant_data =[
+        {name: 'Bread & Bakery', category: 'Padaria', delivery_time: '25 minutos', rating: 4.9},
+        {name: 'Burger Hause', category: 'Hamburgers', delivery_time: '30 minutos', rating: 3.5}
+        ]
+    
+  end
+
+
 Quando("acesso a lista de restaurantes") do
     visit '/restaurants'
+
+   
 end
 
 Então("vejo todas as opções disponíveis") do
     
     restaurante = all('.restaurant-item') # metodo all devolve coleção de elementos
     expect(restaurante.size).to be > 0
+    # expect(restaurante.size).to eql 6
         # puts restaurante.size
         # puts restaurante.class
    
 end
 
 Então("cada restaurante deve exibir sua categoria") do
-     pending # Write code here that turns the phrase above into concrete actions
+    
+    #massa de dados
+    
+
+        restaurants = all('.restaurant-item')
+
+        # restaurant_data.each do |value|
+        @restaurant_data.each_with_index  do |value, index|
+            # puts value[:category]
+            # puts restaurants[index].text
+            expect(restaurants[index]).to have_text value[:category]
+
+        end
+
+
 end
 
 Então("cada restaurante deve exibir o tempo de entrega") do
-      pending # Write code here that turns the phrase above into concrete actions
+      
+    
+
+
+        restaurants = all('.restaurant-item')
+
+        # restaurant_data.each do |value|
+        @restaurant_data.each_with_index  do |value, index|
+            # puts value[:category]
+            # puts restaurants[index].text
+            expect(restaurants[index]).to have_text value[:delivery_time]
+
+        end
+
+
+
 end
 
 Então("cada restaurante deve exibir sua nota de avaliação") do
-      pending # Write code here that turns the phrase above into concrete actions
+    
+
+        restaurants = all('.restaurant-item')
+
+        # restaurant_data.each do |value|
+        @restaurant_data.each_with_index  do |value, index|
+            # puts value[:category]
+            # puts restaurants[index].text
+            expect(restaurants[index]).to have_text value[:rating]
+
+        end
+
 end
